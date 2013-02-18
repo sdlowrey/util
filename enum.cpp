3191439076bda29f2213112a562eb3b1ec9a75e4
Scott Lowrey <sdlowrey@gmail.com>
2013-02-18 09:19:05 -0500

    Code for testing enum and map

---
+++ enum.cpp
@@ -0,0 +1,34 @@
+/* This program must be compiled with -std=c++11 and -fpermissive.
+ * C++11 is required for the list-initialization of newtm.  Permissive
+ * is required to remediate "passing map as 'this' arg of mapped
+ * type.. discards qualifiers".  I have no idea what that means.
+ *
+ * Tested with the following compilers:
+ *  gcc (GCC) 4.7.2 20120921 (Red Hat 4.7.2-2)
+ */
+#include <iostream>
+#include <map>
+#include <string>
+
+using namespace std;
+
+enum newt { eye, ear, nose };
+
+const map<newt, string> newtm = {
+	{eye, "The Eye"},
+	{ear, "The Ear"},
+};
+
+
+string func(newt &n) {
+	return newtm[n];
+}
+
+int main() 
+{
+//	string e = newtm[eye];
+	newt e = ear;
+
+	cout << "eye: " << newtm[eye] << endl;
+	cout << "ear: " << func(e) << endl;
+}
